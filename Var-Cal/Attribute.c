6b85526654c46f25f9d6e1fc9b68725589ac9272
TOUATI Osema <go_touati@esi.dz>
2019-10-09 17:41:50 +0200

    adding / * -

--- Var-Cal/Attribute.c
+++ Var-Cal/Attribute.c
@@ -33,22 +33,66 @@ attribute plus_attribute(attribute x, attribute y) {
 
 attribute mult_attribute(attribute x, attribute y){
   attribute r = new_attribute();
-  /* unconditionally adding integer values */
-  r -> int_val = x -> int_val * y -> int_val;
+  r -> type_val = FLOAT;
+  if (x->type_val == FLOAT) {
+    if (y->type_val == FLOAT) 
+      r -> float_val = x -> float_val * y -> float_val;
+    else
+      r -> float_val = x -> float_val * y -> int_val;
+  }
+  else {
+    if (y->type_val == FLOAT) 
+      r -> float_val = x -> int_val * y -> float_val;
+    else {
+      r -> type_val = INT;
+      r -> int_val = x -> int_val * y -> int_val;
+    }
+  }
+
   return r;
 };
 
 attribute minus_attribute(attribute x, attribute y){
   attribute r = new_attribute();
-  /* unconditionally adding integer values */
-  r -> int_val = x -> int_val - y -> int_val;
+  r -> type_val = FLOAT;
+
+if (x->type_val == FLOAT) {
+    if (y->type_val == FLOAT) 
+      r -> float_val = x -> float_val - y -> float_val;
+    else
+      r -> float_val = x -> float_val - (float) y -> int_val;
+  }
+  else {
+    if (y->type_val == FLOAT) 
+      r -> float_val = (float) x -> int_val - y -> float_val;
+    else {
+      r -> type_val = INT;
+      r -> int_val = x -> int_val - y -> int_val;
+    }
+  }
+
   return r;
+  /* unconditionally adding integer values */
 };
 
 attribute div_attribute(attribute x, attribute y){
   attribute r = new_attribute();
-  /* unconditionally adding integer values */
-  r -> int_val = x -> int_val % y -> int_val;
+  r -> type_val = FLOAT;
+  
+  if (x->type_val == FLOAT) {
+    if (y->type_val == FLOAT) 
+      r -> float_val = x -> float_val / y -> float_val;
+    else
+      r -> float_val = x -> float_val / (float) y -> int_val;
+  }
+  else {
+    if (y->type_val == FLOAT) 
+      r -> float_val = (float) x -> int_val / y -> float_val;
+    else {
+      r -> type_val = INT;
+      r -> int_val = x -> int_val / y -> int_val;
+    }
+  }
   return r;
 };
 
